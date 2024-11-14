100ff3c304662edd7d518aeb3d938e6dd014bac9
Facade <marc.weber54@gmail.com>
2024-11-14 02:00:20 -0700

    Fix config parser

--- src/OpenGLEDConfig.cpp
+++ src/OpenGLEDConfig.cpp
@@ -23,26 +23,26 @@ std::optional<OpenGLEDConfig> OpenGLEDConfig::FromFile(const char* filename)
     if(config["AUDIO_SETTINGS"]){
         return_config.alsa_input_device = config["AUDIO_SETTINGS"]["ALSA_INPUT_DEVICE"].as<std::string>();
 
-        if(config["LED_SETTINGS"]["FREQUENCY_BANDS"]){
-            uint num_bands = config["LED_SETTINGS"]["FREQUENCY_BANDS"].as<uint>();
-            if(config["LED_SETTINGS"]["BAND_CUTOFF_FREQUENCIES"].size() != num_bands + 1){
+        if(config["AUDIO_SETTINGS"]["FREQUENCY_BANDS"]){
+            uint num_bands = config["AUDIO_SETTINGS"]["FREQUENCY_BANDS"].as<uint>();
+            if(config["AUDIO_SETTINGS"]["BAND_CUTOFF_FREQUENCIES"].size() != num_bands + 1){
                 throw std::runtime_error("BAND_CUTOFF_FREQUENCIES needs to give one more cutoff frequency than the number of bands.");
             }
 
             return_config.frequency_bands.clear();
-            for(YAML::Node cutoff_freq : config["LED_SETTINGS"]["BAND_CUTOFF_FREQUENCIES"]){
+            for(YAML::Node cutoff_freq : config["AUDIO_SETTINGS"]["BAND_CUTOFF_FREQUENCIES"]){
                 return_config.frequency_bands.push_back(cutoff_freq.as<float>());
             }
         }
 
-        if(config["LED_SETTINGS"]["SAMPLE_RATE"])
-            return_config.sample_rate = config["LED_SETTINGS"]["SAMPLE_RATE"].as<int>();
+        if(config["AUDIO_SETTINGS"]["SAMPLE_RATE"])
+            return_config.sample_rate = config["AUDIO_SETTINGS"]["SAMPLE_RATE"].as<int>();
 
-        if(config["LED_SETTINGS"]["SAMPLES_PER_PIXEL"])
-            return_config.samples_per_pixel = config["LED_SETTINGS"]["SAMPLES_PER_PIXEL"].as<int>();
+        if(config["AUDIO_SETTINGS"]["SAMPLES_PER_PIXEL"])
+            return_config.samples_per_pixel = config["AUDIO_SETTINGS"]["SAMPLES_PER_PIXEL"].as<int>();
 
-        if(config["LED_SETTINGS"]["PIXELS_PER_IMAGE"])
-            return_config.pixels_per_image = config["LED_SETTINGS"]["PIXELS_PER_IMAGE"].as<int>();
+        if(config["AUDIO_SETTINGS"]["PIXELS_PER_IMAGE"])
+            return_config.pixels_per_image = config["AUDIO_SETTINGS"]["PIXELS_PER_IMAGE"].as<int>();
     }
 
     return_config.shader_folder = config["SHADER_FOLDER"].as<std::string>();
