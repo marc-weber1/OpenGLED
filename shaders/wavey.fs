50561faf5070c5dd104d1019304d84f7c2cc758c
Facade <marc.weber54@gmail.com>
2024-11-22 00:12:45 -0700

    Adjust parameters to get it looking slightly better

--- shaders/wavey.fs
+++ shaders/wavey.fs
@@ -3,6 +3,7 @@ uniform vec2 resolution;
 uniform sampler2D audioTexture;
 
 void main() {
-    float treble_intensity = texture2D(audioTexture, vec2(gl_FragCoord.x / resolution.x, 0.75)).r;
-    gl_FragColor = vec4( treble_intensity, 0, 0, 1);
+    float bass_intensity = texture2D(audioTexture, vec2(gl_FragCoord.x / resolution.x, 0.1)).r;
+    float treble_intensity = sin( (-1.0 * time + gl_FragCoord.x / resolution.x) * 60.0 ) * texture2D(audioTexture, vec2(0.99, 0.3)).r;
+    gl_FragColor = vec4( bass_intensity, treble_intensity, treble_intensity, 1);
 }
